52f29fa5916b579c5779720b7654d7f26c59470b
YouSsef <youssef.elhaddady@usmba.ac.ma>
2021-02-16 16:43:23 +0100

    api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostsApiController;
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/posts', [PostsApiController::class, 'index']);
+
+Route::get('/posts/{post}', [PostsApiController::class, 'show']);
+
+Route::post('/posts', [PostsApiController::class, 'store']);
+
+Route::put('/posts/{post}', [PostsApiController::class, 'update']);
+
+Route::delete('/posts/{post}', [PostsApiController::class, 'destroy']);
